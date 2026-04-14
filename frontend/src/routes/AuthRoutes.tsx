0d8dded859b23f6ad4e4970d83f60f428d68ae0e
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-14 20:02:37 -0300

    Rotas de autenticação

---
+++ frontend/src/routes/AuthRoutes.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router"
+
+import { LoginPage } from "../pages/loginPage"
+import { SignupPage } from "../pages/signupPage"
+
+export function AuthRoutes(){
+    return (
+        <Routes>
+            <Route>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+            </Route>
+        </Routes>
+    )
+}
