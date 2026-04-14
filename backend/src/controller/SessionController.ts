ab404569a1cbf4497c0b00c8f6838a0380ef6278
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-14 20:04:45 -0300

    Controller para criação de usuários feita.

--- backend/src/controller/SessionController.ts
+++ backend/src/controller/SessionController.ts
@@ -1,5 +1,47 @@
+import { type Request, type Response } from "express";
+
+import { prisma } from "../database/prisma";
+
+import { z } from "zod";
+import bcrypt from "bcrypt";
+
 export class SessionController {
-    async create(){
-        
+
+    async create(req: Request, res: Response) {
+        const bodySchema = z.object({
+            name: z.string().min(3, "O nome deve conter no mínimo 3 caracteres"),
+            email: z.email(),
+            password: z.string().min(6, "A senha deve conter no mínimo 6 caracteres"),
+            confirmPassword: z.string(),
+        })
+
+        try {
+
+            const { name, email, password, confirmPassword } = bodySchema.parse(req.body);
+
+            if (password !== confirmPassword) {
+                return res.status(400).json({ message: "Senhas diferentes" });
+            }
+
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const user = await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password: passwordHash,
+                    companyId: "1",
+                }
+            })
+
+            return res.status(201).json(user);
+
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({ message: "Dados inválidos", errors: error.issues });
+            }
+            return res.status(500).json({ message: "Erro ao criar usuário no BACKEND, tente novamente em alguns segundos!" });
+        }
     }
+
 }
