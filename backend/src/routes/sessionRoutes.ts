d97d4b0e18d4c70497345993944fe5f2c992d6c0
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-14 16:07:29 -0300

    Rota de sessão para o usuário fazer login

---
+++ backend/src/routes/sessionRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { SessionController } from "../controller/SessionController.js";
+
+const sessionRoutes = Router();
+const sessionController = new SessionController();
+
+sessionRoutes.post("/", sessionController.create);
+
+export { sessionRoutes }
