97772156ab9da6e3a0373379eeeda6b826c9a722
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-16 19:55:20 -0300

    Página inicial de login

--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -1,4 +1,5 @@
 import { useActionState } from "react";
+import {z} from "zod";
 
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
@@ -8,29 +9,26 @@ export function LoginPage() {
   const [state, formAction, isDisabled] = useActionState(handleSubmit, {
     message: null,
     payload: {
-      name: "",
       email: "",
       password: "",
-      confirmPassword: "",
     }
   });
 
   async function handleSubmit(_: any, formData: FormData) {
 
-    const payload = {
-      name: formData.get("name") as string,
+    const bodySchema = z.object({
+      email: z.email("Email inválido"),
+      password: z.string().min(6, "A senha deve conter no mínimo 6 caracteres"),
+    })
+
+    const payload = bodySchema.parse({
       email: formData.get("email") as string,
       password: formData.get("password") as string,
-      confirmPassword: formData.get("confirmPassword") as string,
-    }
+    });
 
     try {
-
-      if (payload.password !== payload.confirmPassword) {
-        return { message: "Senhas diferentes", payload }
-      }
-
-      const response = await fetch("http://localhost:3333/sessions",{
+    
+      const response = await fetch("http://localhost:3333/sessions/login",{
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -38,22 +36,17 @@ export function LoginPage() {
         body: JSON.stringify(payload)
       })
 
-      console.log(response)
-
       if(!response.ok){
-        return { message: "Erro ao cadastrar, tente novamente em alguns segundos!", payload }
+        return { message: "Erro ao fazer login, tente novamente em alguns segundos!", payload }
       }
 
-      alert(`Name: ${payload.name}\nEmail: ${payload.email}\nPassword: ${payload.password}`);
-
     } catch (error) {
-      return { message: "Erro ao cadastrar, tente novamente em alguns segundos!", payload }
+      return { message: "Erro ao fazer login, tente novamente em alguns segundos!", payload }
     }
 
-  }
+   }
 
   return (
-
     <div className="flex items-center justify-center h-screen">
       <header>
         <h1 className="text-4xl font-bold">Login Page</h1>
@@ -62,36 +55,22 @@ export function LoginPage() {
         <form action={formAction}>
           <div>
             <Input
-              placeholder="name"
-              name="name"
-              defaultValue={state?.payload.name}
-            />
-            <Input
-              placeholder="email"
               name="email"
               type="email"
+              placeholder="Email"
               defaultValue={state?.payload.email}
-
             />
             <Input
-              placeholder="password"
               name="password"
               type="password"
+              placeholder="Password"
               defaultValue={state?.payload.password}
             />
-            <Input
-              placeholder="confirm password"
-              name="confirmPassword"
-              type="password"
-              defaultValue={state?.payload.confirmPassword}
-            />
-
-            <p className="text-red-500 text-sm m-auto">{state?.message}</p>
 
           </div>
           <div>
             <Button
-              title={isDisabled ? "Criando..." : "Criar conta"}
+              title={isDisabled ? "Entrando..." : "Entrar"}
               type="submit"
               disabled={isDisabled}
             />
