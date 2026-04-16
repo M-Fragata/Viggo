a5cc99531c37f00654f65dfc6a399803dd8b5818
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-16 19:55:06 -0300

    Criando controller para login

--- backend/src/controller/SessionController.ts
+++ backend/src/controller/SessionController.ts
@@ -25,6 +25,16 @@ export class SessionController {
 
             const passwordHash = await bcrypt.hash(password, 10);
 
+            const company = await prisma.company.findUnique({
+                where: {
+                    id: "1",
+                }
+            })
+
+            if (!company) {
+                throw new Error("Empresa não encontrada");
+            }
+
             const user = await prisma.user.create({
                 data: {
                     name,
@@ -42,6 +52,44 @@ export class SessionController {
             }
             return res.status(500).json({ message: "Erro ao criar usuário no BACKEND, tente novamente em alguns segundos!" });
         }
+    }
+
+    async login(req: Request, res: Response) {
+
+        const bodySchema = z.object({
+            email: z.email(),
+            password: z.string().min(6, "A senha deve conter no mínimo 6 caracteres"),
+        })
+
+        try {
+
+            const { email, password } = bodySchema.parse(req.body);
+
+            const user = await prisma.user.findUnique({
+                where: {
+                    email
+                }
+            })
+
+            if (!user) {
+                return res.status(400).json({ message: "Email e/ou senha incorretos" });
+            }
+
+            const verifyPassword = await bcrypt.compare(password, user.password);
+
+            if (!verifyPassword) {
+                return res.status(400).json({ message: "Email e/ou senha incorretos" });
+            }
+
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({ message: "Dados inválidos", errors: error.issues });
+            }
+            return res.status(500).json({ message: "Erro ao fazer login no BACKEND, tente novamente em alguns segundos!" });
+        }
+
+
+
     }
 
 }
