773ddb674d724db251612caa1e51a282bdc3e1a2
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-19 18:36:26 -0300

    Adicionando o layout nas rotas do usuário

--- frontend/src/routes/AuthRoutes.tsx
+++ frontend/src/routes/AuthRoutes.tsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router"
 
 import { LoginPage } from "../pages/loginPage"
 import { SignupPage } from "../pages/signupPage"
+import { LayoutPage } from "../pages/layoutPage"
 
 export function AuthRoutes(){
     return (
         <Routes>
-            <Route>
+            <Route path="/" element={<LayoutPage />}>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
             </Route>
