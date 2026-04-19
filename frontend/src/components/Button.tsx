0b5615c5cd7de33d466ed74833fddab9eb434787
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-19 18:35:05 -0300

    Ajuste no component Button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = React.ComponentProps<"button"> & {
 export function Button({ style, type = 'button', title, ...props }: ButtonProps){
     return (
         <button 
-        className={`bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 ${style}`} 
+        className={`bg-emerald-700 border border-white text-white px-4 py-2 rounded-md cursor-pointer hover:bg-emerald-950 hover:scale-105 transition-transform ease-in-out ${style}`} 
         type={type}
         {...props}>
             {title}
