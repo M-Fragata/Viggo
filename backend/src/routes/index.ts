c4a2c29ffe2cce56298421b234a6031fe886d55f
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-22 19:27:45 -0300

    Update index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { sessionRoutes } from "./sessionRoutes.js";
 import { checkinRoutes } from "./checkinRoutes.js";
+import { employeesRoutes } from "./employeesRoutes.js";
 
 const routes = Router();
 
 routes.use("/sessions", sessionRoutes);
 routes.use("/checkins", checkinRoutes);
+routes.use("/employees", employeesRoutes);
 
 export { routes }
