5b30e2d38fced3ec04a5463c60c3a9bc5a2795f2
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-22 19:29:28 -0300

    Update AuthRoutes.tsx

--- frontend/src/routes/AuthRoutes.tsx
+++ frontend/src/routes/AuthRoutes.tsx
@@ -4,12 +4,13 @@ import { LoginPage } from "../pages/loginPage"
 import { SignupPage } from "../pages/signupPage"
 import { LayoutPage } from "../pages/layoutPage"
 
-export function AuthRoutes(){
+export function AuthRoutes() {
     return (
         <Routes>
             <Route path="/" element={<LayoutPage />}>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
+                
             </Route>
         </Routes>
     )
