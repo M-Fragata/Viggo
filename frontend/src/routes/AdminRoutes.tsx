18e2a1b7283b66ca26f7cce9ab88b24a45fdf2cf
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-22 19:29:21 -0300

    Ajuste de páginas no admin routes

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router"
+
+import { DashboardPage } from "../pages/DashboardPage"
+import { LayoutPage } from "../pages/layoutPage"
+
 export function AdminRoutes() {
     return (
-        <div>
-            <h1>Admin Routes</h1>
-        </div>
+        <Routes>
+            <Route path="/" element={<LayoutPage />}>
+                <Route path="/" element={ <DashboardPage />}/>
+            </Route>
+        </Routes>
     )
 }
