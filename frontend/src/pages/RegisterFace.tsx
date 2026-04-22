2f37fc1cb35ffcf63fe6abb2e3d5b7a3e54b7774
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-22 19:29:11 -0300

    Página para registrar a facial do funcionário

---
+++ frontend/src/pages/RegisterFace.tsx
@@ -0,0 +1,50 @@
+// src/pages/RegisterFace.tsx
+import { FaceAuth } from "../components/FaceAuth";
+
+export function RegisterFace() {
+    const handleSaveFace = async (descriptor: Float32Array) => {
+        // Converte para Array normal para enviar via JSON
+        const descriptorArray = Array.from(descriptor);
+
+        const userRaw = localStorage.getItem("@viggo:user")
+        const tokenRaw = localStorage.getItem("@viggo:token")
+
+        if (!userRaw || !tokenRaw) return window.location.href = "/"
+
+        try {
+
+            const user = JSON.parse(userRaw)
+            const token = JSON.parse(tokenRaw)
+            
+            const response = await fetch(`http://localhost:3333/${user.id}`, {
+                method: "PUT",
+                headers: { 
+                    "Content-type": "application/json",
+                    "Authorization": `Bearer ${token}` 
+                },
+                body: JSON.stringify({ faceDescriptor: descriptorArray })
+            })
+    
+            if(!response.ok) return alert("Falha na requisição")
+    
+            user.faceDescriptor = JSON.stringify(descriptorArray)
+            localStorage.setItem("@viggo:user", JSON.stringify(user))
+            
+            alert("Facial registrada com sucesso")
+
+            window.location.href="/"
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    };
+
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center p-4">
+            <h1 className="text-xl font-bold mb-4">Primeiro Acesso: Cadastro Facial</h1>
+            <p className="mb-6 text-gray-600">Precisamos registrar seu rosto para garantir a segurança dos seus pontos.</p>
+            <FaceAuth onAuthenticate={handleSaveFace} />
+        </div>
+    );
+}
