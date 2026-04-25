efe18eb20ba5fb896893824bc07fcd8a741b83e8
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-24 22:34:48 -0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,13 +4,16 @@ import express from 'express';
 import { routes } from './routes/index.js';
 
 const app = express();
-app.use(express.json());
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+console.log(process.env.FRONTEND_URL)
+
+app.use(express.json());
 
 app.use(routes);
 
