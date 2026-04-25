1f3297875e6bd676205844bdf2b717d6cb6fe944
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-24 22:26:02 -0300

    Erro para ajudar a debugar

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,15 @@ import { app } from './app.js';
 
 const PORT = process.env.PORT || 3333;
 
+app.use((err: any, req: any, res: any, next: any) => {
+  console.error("=== ERRO NO SERVIDOR ===");
+  console.error(err); 
+  res.status(500).json({ 
+    message: "Erro interno capturado no middleware global",
+    error: err.message 
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
