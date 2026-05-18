f6d290c5ec4d339559b03b5bc8ec3acf0f51d4ff
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-18 15:54:55 -0300

    Adicionando rotas de usuário para o admin

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -3,11 +3,14 @@ import { Routes, Route } from "react-router"
 import { DashboardPage } from "../pages/DashboardPage"
 import { LayoutPage } from "../pages/layoutPage"
 
+import { UserRoutes } from "./UserRoutes"
+
 export function AdminRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<LayoutPage />}>
-                <Route path="/" element={ <DashboardPage />}/>
+            <Route path="/*" element={<UserRoutes />} />
+            <Route path="/admin" element={<LayoutPage />}>
+                <Route path="/admin" element={<DashboardPage />} />
             </Route>
         </Routes>
     )
