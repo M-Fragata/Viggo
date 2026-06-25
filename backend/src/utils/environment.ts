7470350e112586cbd0d6ac917366caed73f53863
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-06-25 11:41:40 -0300

    Update environment.ts

--- backend/src/utils/environment.ts
+++ backend/src/utils/environment.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import z from "zod"
 
 const environmentSchema = z.object({
-    PORT: z.coerce.number().default(3333),
+    PORT: z.coerce.number(),
     DATABASE_URL: z.string(),
     JWT_SECRET: z.string(),
     FRONTEND_URL: z.string(),
