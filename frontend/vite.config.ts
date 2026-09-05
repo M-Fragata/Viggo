36f730dfeafcd499097570570e040a18ead1dc4a
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-09-05 16:53:05 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   preview: {
-    allowedHosts:['viggo.fragata.me']
+    allowedHosts:['pontofragata.com.br']
   }
 })
