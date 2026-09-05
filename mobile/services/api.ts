093634a2edd63837da6fa5aa36cd40960f47cd82
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-09-05 16:57:07 -0300

    Update api.ts

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,6 +1,6 @@
 import * as SecureStore from 'expo-secure-store';
 
-export const API_URL = 'https://viggoapi.fragata.me';
+export const API_URL = 'https://pontofragata.com.br';
 
 const TOKEN_KEY = '@fragata:token';
 const USER_KEY = '@fragata:user';
